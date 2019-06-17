108f88c02215938f43a8df63511d4f9cfa586a5a
Mr H <dan_d_h@outlook.com>
2019-06-17 23:05:42 +0100

    Unit tests in memory DB setup with first test
    Set up in memory db context and wrote test for getting the comment count

---
+++ NerdNews.Tests/NerdNewsTestBase.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdNews.Data;
+using NerdNews.Data.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdNews.Tests
+{
+    public class NerdNewsTestBase : IDisposable
+    {
+        protected readonly ApplicationDbContext _context;
+        public NerdNewsTestBase()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+
+            _context = new ApplicationDbContext(options);
+
+            var addDate = new List<Comments>();
+
+            addDate.Add(
+                new Comments()
+                {
+                    Id = 1,
+                    Author = "Dan",
+                    CommentDateTime = DateTime.Now,
+                    Message = "Test 1",
+                    PostId = "test"
+                });
+
+            addDate.Add(
+                new Comments()
+                {
+                    Id = 2,
+                    Author = "Dan 2",
+                    CommentDateTime = DateTime.Now.AddMinutes(10),
+                    Message = "Test 2",
+                    PostId = "test"
+                });
+
+
+            _context.Comments.AddRange(addDate);
+
+            _context.SaveChanges();
+
+            _context.Database.EnsureCreated();
+            
+
+        }
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+
+            _context.Dispose();
+        }
+
+        
+    }
+}
--- NerdNews.Tests/UnitTests.cs
+++ NerdNews.Tests/UnitTests.cs
@@ -1,20 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NerdNews.Application;
+using NerdNews.Data;
+using NerdNews.Tests;
 using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
+    public class Tests : NerdNewsTestBase
     {
-        //TODO: Set up in memory dbcontext
-
+        private ProcessData processData;
+        private Logger<ProcessData> logger;
+        
         [SetUp]
         public void Setup()
         {
+            logger = new Logger<ProcessData>(new NullLoggerFactory());
+            processData = new ProcessData(_context, logger);
         }
 
         [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+        public void CheckCommentCount_Test()
+        {     
+            var postId = "test";
+            int count = 2;            
+
+            var getCount = processData.GetCommentCount(postId);
+
+            Assert.AreEqual(count, getCount);
         }
     }
 }
